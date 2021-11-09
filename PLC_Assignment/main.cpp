bd54e6ac9e46a5c5acee2ded590faaf0446714ca
Kenneth Liu <z5254829@ad.unsw.edu.au>
2021-11-09 15:36:51 +1100

    done

--- PLC_Assignment/main.cpp
+++ PLC_Assignment/main.cpp
@@ -3,74 +3,28 @@ using namespace System;
 
 int main() {
 	Galil galil;
+	galil.DigitalOutput(0);
+	float ai = galil.AnalogInput(0);
+	int aii = (int)ai;
+	int aid = (ai - aii)*10;
 
+	for (int i = 0; i < aii; i++) {
+		galil.DigitalBitOutput(1, i);
+	}
+	for (int i = 0; i < aid; i++) {
+		galil.DigitalBitOutput(1, i+8);
+	}
+	Console::ReadKey();
+	galil.AnalogOutput(0, 3);
 	while (1) {
-		char key = Console::ReadKey().KeyChar;
-		switch (key) {
-		case '1':
-			galil.DigitalOutput(0);
-			break;
-		case '2':
-			galil.DigitalByteOutput(1, 1);
-			break;
-		case '3':
-			galil.DigitalBitOutput(1, 7);
-			break;
-		case '4':
-			printf("%u\n", galil.DigitalInput());
-			break;
-		case '5':
-			printf("%u\n", galil.DigitalByteInput(0));
-			break;
-		case '6':
-			printf("%u\n", galil.DigitalBitInput(7));
-			break;
-		case '7':
-			galil.DigitalBitOutput(1, 10);
-			galil.DigitalBitOutput(1, 0);
-			printf("%d\n", galil.CheckSuccessfulWrite());
-			break;
-		case '8':
-			printf("%f\n", galil.AnalogInput(0));
-			break;
-		case '9':
-			galil.AnalogOutput(0, 5);
-			break;
-		case '0':
-			galil.AnalogInputRange(0, 1);
-			printf("%d\n", galil.CheckSuccessfulWrite());
-			break;
-		case 'q':
-			galil.WriteEncoder();
-			printf("%d\n", galil.CheckSuccessfulWrite());
-			break;
-		case 'w':
-			printf("%d\n", galil.ReadEncoder());
-			break;
-		case 'e':
-			galil.setSetPoint(5);
-			printf("%d\n", galil.CheckSuccessfulWrite());
-			break;
-		case 'r':
-			galil.setKd(1);
-			printf("%d\n", galil.CheckSuccessfulWrite());
-			break;
-		case 't':
-			galil.setKp(2);
-			printf("%d\n", galil.CheckSuccessfulWrite());
-			break;
-		case 'y':
-			galil.setKi(3);
-			printf("%d\n", galil.CheckSuccessfulWrite());
-			break;
-		case 'u':
-			galil.AnalogOutput(0, 0);
-			break;
-		case 'i':
-			std::cout << '\n' << galil << '\n';
-			break;
-		case 'o':
-			galil.AnalogInputRange(0, 3);
+		int d = galil.ReadEncoder();
+		for (int i = 0; i < 8; i++) {
+			int val = (d >> i) & 1;
+			galil.DigitalBitOutput(val, i);
+		}
+		for (int i = 0; i < 8; i++) {
+			int val = (d >> i) & 1;
+			galil.DigitalBitOutput(val, i+8);
 		}
 	}
 }
